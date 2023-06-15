d6fd44ab244c629399887f09eb28cd0fb85d5ca7
Abdelrahman Kamal <abdelrahman.k212@gmail.com>
2023-06-15 05:47:21 +0300

    Renaming bit masking macro like functions

--- LIB/BIT_MATH.h
+++ LIB/BIT_MATH.h
@@ -2,7 +2,7 @@
 /* Author            : Abdelrahman Kamal Eldean                                                        */
 /* Version           : V0.0.0                                                                          */
 /* Data              : 16 May 2023                                                                     */
-/* Description       : LBIT_MATH.h                                                                     */
+/* Description       : BIT_MATH.h                                                                     */
 /*******************************************************************************************************/
 /*******************************************************************************************************/
 /*                                            File Guard		                                       */
@@ -12,17 +12,17 @@
 /*******************************************************************************************************/
 /*                                        Function Like Macros	     	                               */
 /*******************************************************************************************************/
-#define SET_BIT(REG , BITNUM) ((REG) |= (1U<<(BITNUM)))
-#define CLR_BIT(REG , BITNUM) ((REG) &= ~(1U<<BITNUM))
-#define TOG_BIT(REG , BITNUM) ((REG) ^= (1U<<(BITNUM)))
-#define GET_BIT(REG , BITNUM) (((REG) >> (BITNUM)) & 1U)
+#define SET_BIT(REG, BITNUM)        ((REG) |= (1U<<(BITNUM)))
+#define CLR_BIT(REG, BITNUM)        ((REG) &= ~(1U<<BITNUM))
+#define TOG_BIT(REG, BITNUM)        ((REG) ^= (1U<<(BITNUM)))
+#define GET_BIT(REG, BITNUM)        (((REG) >> (BITNUM)) & 1U)
 
-#define Set_BITS(REG,MSK)			((REG) |=  (MSK))
-#define Clr_BITS(REG,MSK)			((REG) &= ~(MSK))
-#define Toggle_BITS(REG,MSK)		((REG) ^=  (MSK))
+#define SET_BITS(REG, MSK)			((REG) |=  (MSK))
+#define CLR_BITS(REG, MSK)			((REG) &= ~(MSK))
+#define TOG_BITS(REG, MSK)		    ((REG) ^=  (MSK))
 
-#define Set_ALL_BITS(REG)			((REG) =  (0xFFFFFFFF))
-#define Clr_ALL_BITS(REG)			((REG) =  (0x00000000))
-#define Toggle_ALL_BITS(REG)		((REG) ^= (0xFFFFFFFF))
+#define SET_ALL_BITS(REG)			((REG) =  (0xFFFFFFFF))
+#define CLR_ALL_BITS(REG)			((REG) =  (0x00000000))
+#define TOG_ALL_BITS(REG)		    ((REG) ^= (0xFFFFFFFF))
 
 #endif /* BIT_MATH_H_ */
